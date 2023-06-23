e452052689609dafa19abd24bc107f356e667985
Olexandr Vaidych <10lexandr.vaidych@gmail.com>
2023-06-23 11:26:25 +0300

    weight coefficients added

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ class MainWindow(QMainWindow):
         self.terms_label = QLabel("Terms: T2, T2, T4, T3, T1, T1", self)
         self.terms_label.setGeometry(50, 75, 200, 30)
 
+        self.weight_coeffs_label = QLabel("Weight coefficients: 8, 7, 9, 8, 6, 5", self)
+        self.weight_coeffs_label.setGeometry(50, 100, 200, 30)
+
     def visualize_input_data(self):
         criteria = ['K1', 'K2', 'K3', 'K4', 'K5', 'K6']
         values = [0.8, 0.7, 0.9, 0.8, 0.6, 0.5]
