ecfabaf0376183f013ff639d7115a7c1fc279a1c
Olexandr Vaidych <10lexandr.vaidych@gmail.com>
2023-06-23 10:47:44 +0300

    terms added

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 import matplotlib.pyplot as plt
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        terms = 0.8, 0.7, 0.9, 0.8, 0.6, 0.5
+
         self.setGeometry(100, 100, 300, 200)
 
         self.visualize_button = QPushButton("Visualize input data", self)
         self.visualize_button.setGeometry(50, 25, 150, 30)
         self.visualize_button.clicked.connect(self.visualize_input_data)
+
+        self.terms_label = QLabel("Terms: 0.8, 0.7, 0.9, 0.8, 0.6, 0.5", self)
+        self.terms_label.setGeometry(50, 75, 200, 30)
 
     def visualize_input_data(self):
         criteria = ['K1', 'K2', 'K3', 'K4', 'K5', 'K6']
